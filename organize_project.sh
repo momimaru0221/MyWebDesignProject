61565489779a67092e1fd11143a97e3ecd47bed5
momimaru <momimaru0221@gmail.com>
2024-09-08 13:11:34 +0900

    Auto commit after organizing project

--- organize_project.sh
+++ organize_project.sh
@@ -5,20 +5,6 @@ cd "$(dirname "$0")"
 
 # プロジェクト構造の整理
 
-# コンポーネントフォルダを統一
-echo "Renaming {components} to shared_components in css and html directories..."
-mv src/css/\{components\} src/css/shared_components
-mv src/html/\{components\} src/html/shared_components
-
-# pathGeneratorTest.* ファイルを tests/demo に移動
-echo "Creating tests/demo directory..."
-mkdir -p tests/demo
-
-echo "Moving pathGeneratorTest files to tests/demo..."
-mv src/css/pathGeneratorTest.css tests/demo/
-mv src/html/pathGeneratorTest.html tests/demo/
-mv src/js/pathGeneratorTest.js tests/demo/
-
 # Gitの自動更新処理
 
 # 最新の変更を取得
